94a06d898e0e714d318353c13012f2f2c5540ea7
raha <elahe.dstn@gmail.com>
2020-04-09 16:15:00 +0430

    go formatted files

--- bank/bank.go
+++ bank/bank.go
@@ -13,9 +13,10 @@ func New() Bank {
 	return Bank{words: []string{"parham", "raha", "amirkabir", "alibaba"}}
 }
 
+// nolint: gosec
 func (b Bank) Choose() string {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Int() % len(b.words)
+
 	return b.words[r]
 }
-
--- main.go
+++ main.go
@@ -18,6 +18,5 @@ func main() {
 		}
 
 		p.Play()
-
 	}
 }
